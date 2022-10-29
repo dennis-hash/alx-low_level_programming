65878266999d043cb0d066c649cc97fdef683e1d
Dennis <ndungudennis250@gmail.com>
2022-10-28 19:45:48 -0700

    w

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int main(void)
@@ -13,7 +14,7 @@ int main(void)
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
-   * - only one statemen
+   * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
   *(p + 5) = 98;
